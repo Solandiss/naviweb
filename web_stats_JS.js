8218ecd5402481cbc3d6c225c4abf7655f9ba744
Ghin <152283493+Solandiss@users.noreply.github.com>
2024-01-20 01:10:31 +0800

    Add files via upload

---
+++ web_stats_JS.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Display the tagline pop-up after a delay (e.g., 2 seconds)
+    setTimeout(function () {
+        alert('Welcome to Navs Personal Web'); // You can replace this with a custom pop-up or modal
+    }, 2000);
+});
